460253b866d3a9033f4b777500d21e02e05b21cc
André Kovac <me@andrekovac.com>
2021-11-25 08:17:02 +0100

    Create a simple component with TSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const WasNeues = () => <div>Chinese: 世界，你好！</div>;
+
+const MyFirstComponent = () => (
+  <>
+    <div style={{ backgroundColor: "blue", color: "white" }}>
+      Hallo Welt {Math.random() > 0.5 ? "!" : "?"}
+    </div>
+    <div>English: Hello, world!</div>
+    <WasNeues />
+  </>
+);
+
+// Declarative
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyFirstComponent />
     </div>
   );
 }
